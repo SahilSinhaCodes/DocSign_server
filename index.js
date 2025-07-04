bf3b403604d841c77ae1587f61624b025dad7090
sahilsinhacodes <sinhasahil423@gmail.com>
2025-07-04 15:14:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/api/signatures', signatureRoutes);
 // Connect DB and start server
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 });
